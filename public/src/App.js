ed6f67af07052569593405714d4c132133209162
Marcus Cezar <42722469+marcub@users.noreply.github.com>
2025-05-03 23:19:48 -0300

    Ajustando dependências do projeto

--- public/src/App.js
+++ public/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import Register from './pages/Register'
 import Login from './pages/Login'
 import Chat from './pages/Chat'
@@ -9,6 +9,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/'element={<Navigate to="/login" />} />
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
         <Route path='/setAvatar' element={<SetAvatar />} />
